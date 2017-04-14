eb4119748a40d468e421a06ebbbcb2921f61d533
James Gardiner <jamesgardiner075@gmail.com>
2017-04-14 15:04:14 +0100

    Add error handling and app listen

--- app.js
+++ app.js
@@ -120,3 +120,42 @@ app.post('/videos'. videos.hasAuthorization, upload.single('video'),
 app.post('/images', images.hasAuthorization, upload.single('image'),
         images.uploadImage);
 app.get('/images-gallery', images.hasAuthorization, images.show);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' +
+  server.address().port);
+});
